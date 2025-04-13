403d8ac022c3ecb337fc8ed3c8d2c7b93bb3d80f
tarek fawzy <tf5234045@gmail.com>
2025-04-14 01:21:57 +0200

    update media queries for better mobile && tablet experience

--- src/components/StaticCard.jsx
+++ src/components/StaticCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const StaticCard = ({ num, text }) => {
   return (
-    <div className="bg-gray-10 rounded-md p-3 last:col-span-2 sm:last:col-span-1">
+    <div className="bg-gray-10 rounded-md p-3 last:col-span-2 lg:last:col-span-1">
       <p className="text-white text-2xl font-semibold">{num}</p>
       <p className="mt-1 text-gray-60 text-sm">{text}</p>
     </div>
--- src/pages/AboutPage/Hero.jsx
+++ src/pages/AboutPage/Hero.jsx
@@ -7,21 +7,21 @@ const Hero = () => {
   return (
     <div className="mt-5">
       <div className="flex items-start md:items-center justify-between gap-5 flex-col-reverse md:flex-row">
-        <div>
+        <div className="md:w-1/2">
           <SeactionHeader
             title="Our Journey"
             subTiltle="Our story is one of continuous growth and evolution. We started as a small team with big dreams, determined to create a real estate platform that transcended the ordinary. Over the years, we've expanded our reach, forged valuable partnerships, and gained the trust of countless clients."
           />
 
-          <div className="grid mt-5 grid-cols-2 sm:grid-cols-3 gap-2 md:mx-4">
+          <div className="grid mt-5 grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-2 md:mx-4">
             {statics.map((item, idx) => (
               <StaticCard key={idx} num={item.number} text={item.title} />
             ))}
           </div>
         </div>
 
-        <div className="size-full lg:w-lg about relative z-[2] rounded-lg overflow-hidden border border-gray-30/50">
-            <img src="public/about-bg.png" className="size-full object-cover object-center" alt="about img" />
+        <div className="about relative z-[2] rounded-lg overflow-hidden border border-gray-30/50">
+            <img src="public/about-bg.png" className="object-cover object-center md:w-[90%]" alt="about img" />
         </div>
       </div>
     </div>
